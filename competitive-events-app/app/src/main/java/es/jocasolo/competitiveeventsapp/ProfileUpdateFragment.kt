18e1ad17b1d170a3ee566a71e2c4ada35e375dc9
jocasolo <carlos.solislojo@gmail.com>
2022-01-05 19:26:23 +0100

    [APP] Datepicker for date input

--- competitive-events-app/app/src/main/java/es/jocasolo/competitiveeventsapp/ProfileUpdateFragment.kt
+++ competitive-events-app/app/src/main/java/es/jocasolo/competitiveeventsapp/ProfileUpdateFragment.kt
@@ -1,5 +1,6 @@
-package es.jocasolo.competitiveeventsapp
+ package es.jocasolo.competitiveeventsapp
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -18,18 +19,18 @@ import es.jocasolo.competitiveeventsapp.service.ServiceBuilder
 import es.jocasolo.competitiveeventsapp.service.UserService
 import es.jocasolo.competitiveeventsapp.singleton.UserAccount
 import es.jocasolo.competitiveeventsapp.singleton.UserInfo
-import es.jocasolo.competitiveeventsapp.utils.Message
-import es.jocasolo.competitiveeventsapp.utils.MyDialog
-import es.jocasolo.competitiveeventsapp.utils.MyUtils
+import es.jocasolo.competitiveeventsapp.utils.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.net.HttpURLConnection
 import java.text.SimpleDateFormat
+import java.util.*
 
-class ProfileUpdateFragment : Fragment() {
+ class ProfileUpdateFragment : Fragment() {
 
     private val userService = ServiceBuilder.buildService(UserService::class.java)
+    private var sdf : SimpleDateFormat? = null
     private var txtName : TextView? = null
     private var txtSurname : TextView? = null
     private var txtBirthDate : TextView? = null
@@ -37,8 +38,8 @@ class ProfileUpdateFragment : Fragment() {
     private var spinner : ProgressBar? = null
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile_update, container, false)
@@ -49,6 +50,7 @@ class ProfileUpdateFragment : Fragment() {
         txtName = view.findViewById(R.id.txt_update_name)
         txtSurname = view.findViewById(R.id.txt_update_surname)
         txtBirthDate = view.findViewById(R.id.txt_update_birthdate)
+        txtBirthDate?.setOnClickListener(View.OnClickListener { showDatePicker() })
         txtDescription = view.findViewById(R.id.txt_update_description)
         spinner = view.findViewById(R.id.spn_update_user)
 
@@ -58,8 +60,31 @@ class ProfileUpdateFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
-    private fun initFields(user : UserDTO?) {
-        var sdf : SimpleDateFormat? = SimpleDateFormat(getString(R.string.sdf_date))
+    private fun showDatePicker(){
+
+        val calendar = Calendar.getInstance()
+        if (txtBirthDate?.text != null){
+            calendar.time = sdf?.parse(txtBirthDate?.text.toString())
+        } else {
+            calendar.time = Date()
+        }
+
+        val listener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth -> updateBirthDayDate(
+            year,
+            month,
+            dayOfMonth
+        ) }
+        val dialog = DatePickerDialog(requireContext(), listener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH))
+        dialog.show()
+    }
+
+    private fun updateBirthDayDate(year: Int, month: Int, dayOfMonth: Int) {
+        txtBirthDate?.text = "$dayOfMonth-$month-$year"
+    }
+
+    private fun initFields(user: UserDTO?) {
+
+        sdf = SimpleDateFormat(getString(R.string.sdf_date))
 
         user?.avatar?.let {
             Picasso.get().load(user.avatar!!.url).into(view?.findViewById<ImageView>(R.id.img_update_avatar))
@@ -86,41 +111,48 @@ class ProfileUpdateFragment : Fragment() {
         }
     }
 
-    private fun update(view : View){
+    private fun update(view: View){
         MyUtils.closeKeyboard(this.requireContext(), view)
         if(validate()) {
 
-            var sdfApi : SimpleDateFormat? = SimpleDateFormat(getString(R.string.sdf_api_date))
-            var sdfView : SimpleDateFormat? = SimpleDateFormat(getString(R.string.sdf_date))
-            var birthDate = sdfView?.parse(txtBirthDate?.text.toString())
+            val sdfApi : SimpleDateFormat = SimpleDateFormat(getString(R.string.sdf_api_date))
+            val birthDate = sdf?.parse(txtBirthDate?.text.toString())
 
             spinner?.visibility = View.VISIBLE
 
             val actualUserDto = UserInfo.getInstance(requireContext()).getUserDTO();
             val updatedUserDto = UserPutDTO(
-                    null,
-                    txtName?.text.toString(),
-                    txtSurname?.text.toString(),
-                    txtDescription?.text.toString(),
-                    sdfApi?.format(birthDate)
+                null,
+                txtName?.text.toString(),
+                txtSurname?.text.toString(),
+                txtDescription?.text.toString(),
+                sdfApi.format(birthDate!!)
             )
 
-            userService.update(actualUserDto?.id.toString(), updatedUserDto, UserAccount.getInstance(requireContext()).getToken()).enqueue(object : Callback<Void> {
+            userService.update(
+                actualUserDto?.id.toString(), updatedUserDto, UserAccount.getInstance(
+                    requireContext()
+                ).getToken()
+            ).enqueue(object : Callback<Void> {
                 override fun onResponse(call: Call<Void>, response: Response<Void>) {
-                    if(response.code() == HttpURLConnection.HTTP_OK) {
+                    if (response.code() == HttpURLConnection.HTTP_OK) {
                         actualUserDto?.name = updatedUserDto.name.toString();
                         actualUserDto?.surname = updatedUserDto.surname.toString();
                         showSuccessDialog(getString(R.string.success_updated_user))
                     } else {
                         try {
-                            val errorDto = Gson().fromJson(response.errorBody()?.string(), ErrorDTO::class.java) as ErrorDTO
+                            val errorDto = Gson().fromJson(
+                                response.errorBody()?.string(),
+                                ErrorDTO::class.java
+                            ) as ErrorDTO
                             showErrorDialog(getString(Message.forCode(errorDto.message)))
-                        } catch (e : Exception) {
+                        } catch (e: Exception) {
                             showErrorDialog(getString(R.string.error_api_undefined))
                         }
                     }
                     spinner?.visibility = View.INVISIBLE
                 }
+
                 override fun onFailure(call: Call<Void>, t: Throwable) {
                     showErrorDialog(getString(R.string.error_api_undefined))
                     spinner?.visibility = View.INVISIBLE
@@ -146,11 +178,11 @@ class ProfileUpdateFragment : Fragment() {
         return result
     }
 
-    private fun showSuccessDialog(message : String) {
+    private fun showSuccessDialog(message: String) {
         MyDialog.message(this, getString(R.string.success_action_title), message)
     }
 
-    private fun showErrorDialog(message : String) {
+    private fun showErrorDialog(message: String) {
         MyDialog.message(this, getString(R.string.error_title), message)
     }
 
